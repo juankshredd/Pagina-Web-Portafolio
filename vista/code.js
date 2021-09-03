b81ca0513dfbdb5a7d30bbaceb4919e27a9f8fea
juankshredd <juankmarcelo@gmail.com>
2021-09-03 16:17:38 -0500

    implementacion login en proceso

--- vista/code.js
+++ vista/code.js
@@ -10,35 +10,61 @@
 //     console.log("server on port 3000");
 // });
 
+
+// const express= require('express');
+// const app= express();
+
+// app.listen(3000, () => {
+//     console.log("server on port 3000");
+// });
+
+// app.use(express.json());
+// app.get('/user', (req,res)=> {
+//     res.json({
+//         'Nombre':'Juank',
+//         'Apellido': 'Bohorquez'
+//     });
+// });
+
+// app.post('/user', (req,res)=> {
+//     console.log(req.body);
+//     res.send('POST REQUEST RECEIVED');
+// });
+
+// app.put('/contact', (req,res)=> {
+//     res.send('UPDATE REQUEST RECEIVED');
+// });
+
+// app.delete('/test', (req,res)=> {
+//     res.send('<H1>DELETE REQUEST RECEIVED</H1>');
+// });
+
+// app.listen(5000, () => {
+//     console.log("server on port 5000");
+// });
+
 const express= require('express');
 const app= express();
 
-app.listen(3000, () => {
-    console.log("server on port 3000");
+app.set('view engine', 'html');
+
+app.get("/", (req,res)=>{
+    // si se inicia sesion muestra
+    
+    // si no, redirige a login
 });
 
-app.use(express.json());
-app.get('/user', (req,res)=> {
-    res.json({
-        'Nombre':'Juank',
-        'Apellido': 'Bohorquez'
-    });
+
+
+app.get("/login", (req,res)=>{
+    // mostrar formulario de login
+    res.render("login");
 });
 
-app.post('/user', (req,res)=> {
-    console.log(req.body);
-    res.send('POST REQUEST RECEIVED');
+app.post("/login", (req, res)=>{
+    // recibir credenciales para inicar sesion 
+    
 });
 
-app.put('/contact', (req,res)=> {
-    res.send('UPDATE REQUEST RECEIVED');
-});
-
-app.delete('/test', (req,res)=> {
-    res.send('<H1>DELETE REQUEST RECEIVED</H1>');
-});
-
-app.listen(5000, () => {
-    console.log("server on port 5000");
-});
+app.listen(3000, ()=> console.log("Server Started On Port 3000"));
 
